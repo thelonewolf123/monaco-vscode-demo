c7a59973adccafb7322f7017a6ca20257543e828
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-10-10 01:08:09 +0530

    editor working

--- src/setup.ts
+++ src/setup.ts
@@ -9,6 +9,7 @@ import {
     LogLevel,
     StandaloneServices
 } from 'vscode/services'
+import ExtensionHostWorker from 'vscode/workers/extensionHost.worker?worker'
 
 import getAccessibilityServiceOverride from '@codingame/monaco-vscode-accessibility-service-override'
 import getAudioCueServiceOverride from '@codingame/monaco-vscode-audio-cue-service-override'
@@ -22,11 +23,14 @@ import getLanguageDetectionWorkerServiceOverride from '@codingame/monaco-vscode-
 import LanguageDetectionWorker from '@codingame/monaco-vscode-language-detection-worker-service-override/worker?worker'
 import getLanguagesServiceOverride from '@codingame/monaco-vscode-languages-service-override'
 import getLifecycleServiceOverride from '@codingame/monaco-vscode-lifecycle-service-override'
+import getMarkersServiceOverride from '@codingame/monaco-vscode-markers-service-override'
 import getModelServiceOverride from '@codingame/monaco-vscode-model-service-override'
 import getNotificationServiceOverride from '@codingame/monaco-vscode-notifications-service-override'
+import getOutputServiceOverride from '@codingame/monaco-vscode-output-service-override'
 import OutputLinkComputerWorker from '@codingame/monaco-vscode-output-service-override/worker?worker'
 import getPreferencesServiceOverride from '@codingame/monaco-vscode-preferences-service-override'
 import getQuickAccessServiceOverride from '@codingame/monaco-vscode-quickaccess-service-override'
+import getSearchServiceOverride from '@codingame/monaco-vscode-search-service-override'
 import getSnippetServiceOverride from '@codingame/monaco-vscode-snippets-service-override'
 import getStorageServiceOverride, {
     BrowserStorageService
@@ -43,11 +47,10 @@ import getViewsServiceOverride, {
 import getWorkspaceTrustOverride from '@codingame/monaco-vscode-workspace-trust-service-override'
 
 import { openNewCodeEditor } from './editor'
-import { toCrossOriginWorker } from './tools/workers'
+import { toCrossOriginWorker, toWorkerConfig } from './tools/workers'
 
 // Workers
 export type WorkerLoader = () => Worker
-
 const workerLoaders: Partial<Record<string, WorkerLoader>> = {
     editorWorkerService: () => new (toCrossOriginWorker(EditorWorker))(),
     textMateWorker: () => new (toCrossOriginWorker(TextMateWorker))(),
@@ -73,7 +76,7 @@ const remotePath =
 
 // Override services
 await initializeMonacoService({
-    ...getExtensionServiceOverride(),
+    ...getExtensionServiceOverride(toWorkerConfig(ExtensionHostWorker)),
     ...getModelServiceOverride(),
     ...getNotificationServiceOverride(),
     ...getDialogsServiceOverride(),
@@ -106,6 +109,9 @@ await initializeMonacoService({
         shouldUseGlobalPicker: (_editor, isStandalone) =>
             !isStandalone && isEditorPartVisible()
     }),
+    ...getOutputServiceOverride(),
+    ...getSearchServiceOverride(),
+    ...getMarkersServiceOverride(),
     ...getAccessibilityServiceOverride(),
     ...getLanguageDetectionWorkerServiceOverride(),
     ...getStorageServiceOverride(),
