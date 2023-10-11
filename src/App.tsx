ef9ff08e032b8bd8d9dd8bec72149e31280c92d2
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-10-12 02:21:18 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,6 @@
 import './setup'
-import './features/intellisense'
-import '@codingame/monaco-vscode-clojure-default-extension'
-import '@codingame/monaco-vscode-coffeescript-default-extension'
-import '@codingame/monaco-vscode-cpp-default-extension'
-import '@codingame/monaco-vscode-csharp-default-extension'
-import '@codingame/monaco-vscode-css-default-extension'
-import '@codingame/monaco-vscode-diff-default-extension'
-import '@codingame/monaco-vscode-fsharp-default-extension'
-import '@codingame/monaco-vscode-go-default-extension'
-import '@codingame/monaco-vscode-groovy-default-extension'
-import '@codingame/monaco-vscode-html-default-extension'
-import '@codingame/monaco-vscode-java-default-extension'
-import '@codingame/monaco-vscode-javascript-default-extension'
-import '@codingame/monaco-vscode-json-default-extension'
-import '@codingame/monaco-vscode-julia-default-extension'
-import '@codingame/monaco-vscode-lua-default-extension'
-import '@codingame/monaco-vscode-markdown-basics-default-extension'
-import '@codingame/monaco-vscode-objective-c-default-extension'
-import '@codingame/monaco-vscode-perl-default-extension'
-import '@codingame/monaco-vscode-php-default-extension'
-import '@codingame/monaco-vscode-powershell-default-extension'
-import '@codingame/monaco-vscode-python-default-extension'
-import '@codingame/monaco-vscode-r-default-extension'
-import '@codingame/monaco-vscode-ruby-default-extension'
-import '@codingame/monaco-vscode-rust-default-extension'
-import '@codingame/monaco-vscode-scss-default-extension'
-import '@codingame/monaco-vscode-shellscript-default-extension'
-import '@codingame/monaco-vscode-sql-default-extension'
-import '@codingame/monaco-vscode-swift-default-extension'
-import '@codingame/monaco-vscode-typescript-basics-default-extension'
-import '@codingame/monaco-vscode-vb-default-extension'
-import '@codingame/monaco-vscode-xml-default-extension'
-import '@codingame/monaco-vscode-yaml-default-extension'
-import '@codingame/monaco-vscode-theme-defaults-default-extension'
-import '@codingame/monaco-vscode-theme-seti-default-extension'
-import '@codingame/monaco-vscode-references-view-default-extension'
-import '@codingame/monaco-vscode-search-result-default-extension'
-import '@codingame/monaco-vscode-configuration-editing-default-extension'
-import '@codingame/monaco-vscode-markdown-math-default-extension'
-import '@codingame/monaco-vscode-npm-default-extension'
-import '@codingame/monaco-vscode-media-preview-default-extension'
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import {
     attachPart,
@@ -53,12 +12,17 @@ import {
 function App() {
     const editorRef = useRef<HTMLDivElement>(null)
     const explorerRef = useRef<HTMLDivElement>(null)
-    const auxRef = useRef<HTMLDivElement>(null)
-    // const editorInstance = useRef<editor.IStandaloneCodeEditor>()
+    const [ideReady, setIdeReady] = useState(false)
+    const isInitialized = useRef(false)
+
     useEffect(() => {
+        setIdeReady(true)
+    }, [])
+    useEffect(() => {
+        if (isInitialized.current) return
+
         async function handle() {
-            if (!editorRef.current || !explorerRef.current || !auxRef.current)
-                return
+            if (!editorRef.current || !explorerRef.current || !ideReady) return
 
             const part = Parts.EDITOR_PART
             attachPart(part, editorRef.current)
@@ -84,52 +48,34 @@ function App() {
                 explorerRef.current.style.display = visible ? 'block' : 'none'
             })
 
-            const auxPart = Parts.STATUSBAR_PART
-            attachPart(auxPart, auxRef.current)
-
-            if (!isPartVisibile(explorerPart)) {
-                auxRef.current.style.display = 'none'
-            }
-
-            onPartVisibilityChange(explorerPart, (visible) => {
-                if (!auxRef.current) return
-                auxRef.current.style.display = visible ? 'block' : 'none'
-            })
+            isInitialized.current = true
         }
 
         handle()
-    }, [])
+    }, [ideReady])
     return (
-        <>
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'row'
+            }}
+        >
             <div
+                ref={editorRef}
                 style={{
-                    display: 'flex',
-                    flexDirection: 'row'
-                }}
-            >
-                <div
-                    ref={editorRef}
-                    style={{
-                        height: '90vh',
-                        width: '50%'
-                    }}
-                ></div>
-
-                <div
-                    ref={explorerRef}
-                    style={{
-                        height: '90vh',
-                        width: '50%'
-                    }}
-                ></div>
-            </div>
-            <div
-                ref={auxRef}
-                style={{
-                    height: '20vh'
+                    height: '90vh',
+                    width: '50%'
                 }}
             ></div>
-        </>
+
+            <div
+                ref={explorerRef}
+                style={{
+                    height: '90vh',
+                    width: '50%'
+                }}
+            ></div>
+        </div>
     )
 }
 
--- src/setup.ts
+++ src/setup.ts
@@ -1,3 +1,49 @@
+import '@codingame/monaco-vscode-clojure-default-extension'
+import '@codingame/monaco-vscode-coffeescript-default-extension'
+import '@codingame/monaco-vscode-cpp-default-extension'
+import '@codingame/monaco-vscode-csharp-default-extension'
+import '@codingame/monaco-vscode-css-default-extension'
+import '@codingame/monaco-vscode-diff-default-extension'
+import '@codingame/monaco-vscode-fsharp-default-extension'
+import '@codingame/monaco-vscode-go-default-extension'
+import '@codingame/monaco-vscode-groovy-default-extension'
+import '@codingame/monaco-vscode-html-default-extension'
+import '@codingame/monaco-vscode-java-default-extension'
+import '@codingame/monaco-vscode-javascript-default-extension'
+import '@codingame/monaco-vscode-json-default-extension'
+import '@codingame/monaco-vscode-julia-default-extension'
+import '@codingame/monaco-vscode-lua-default-extension'
+import '@codingame/monaco-vscode-markdown-basics-default-extension'
+import '@codingame/monaco-vscode-objective-c-default-extension'
+import '@codingame/monaco-vscode-perl-default-extension'
+import '@codingame/monaco-vscode-php-default-extension'
+import '@codingame/monaco-vscode-powershell-default-extension'
+import '@codingame/monaco-vscode-python-default-extension'
+import '@codingame/monaco-vscode-r-default-extension'
+import '@codingame/monaco-vscode-ruby-default-extension'
+import '@codingame/monaco-vscode-rust-default-extension'
+import '@codingame/monaco-vscode-scss-default-extension'
+import '@codingame/monaco-vscode-shellscript-default-extension'
+import '@codingame/monaco-vscode-sql-default-extension'
+import '@codingame/monaco-vscode-swift-default-extension'
+import '@codingame/monaco-vscode-typescript-basics-default-extension'
+import '@codingame/monaco-vscode-vb-default-extension'
+import '@codingame/monaco-vscode-xml-default-extension'
+import '@codingame/monaco-vscode-yaml-default-extension'
+import '@codingame/monaco-vscode-theme-defaults-default-extension'
+import '@codingame/monaco-vscode-theme-seti-default-extension'
+import '@codingame/monaco-vscode-references-view-default-extension'
+import '@codingame/monaco-vscode-search-result-default-extension'
+import '@codingame/monaco-vscode-configuration-editing-default-extension'
+import '@codingame/monaco-vscode-markdown-math-default-extension'
+import '@codingame/monaco-vscode-npm-default-extension'
+import '@codingame/monaco-vscode-media-preview-default-extension'
+import '@codingame/monaco-vscode-json-language-features-default-extension'
+import '@codingame/monaco-vscode-typescript-language-features-default-extension'
+import '@codingame/monaco-vscode-html-language-features-default-extension'
+import '@codingame/monaco-vscode-css-language-features-default-extension'
+import '@codingame/monaco-vscode-markdown-language-features-default-extension'
+
 import * as monaco from 'monaco-editor'
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker.js?worker'
 import { initialize as initializeVscodeExtensions } from 'vscode/extensions'
@@ -74,58 +120,62 @@ const remoteAuthority = params.get('remoteAuthority') ?? undefined
 const remotePath =
     remoteAuthority != null ? params.get('remotePath') ?? undefined : undefined
 
-// Override services
-await initializeMonacoService({
-    ...getExtensionServiceOverride(toWorkerConfig(ExtensionHostWorker)),
-    ...getModelServiceOverride(),
-    ...getNotificationServiceOverride(),
-    ...getDialogsServiceOverride(),
-    ...getConfigurationServiceOverride(
-        remotePath == null
-            ? monaco.Uri.file('/tmp')
-            : {
-                  id: 'remote-workspace',
-                  uri: monaco.Uri.from({
-                      scheme: 'vscode-remote',
-                      path: remotePath,
-                      authority: remoteAuthority
-                  })
-              }
-    ),
-    ...getKeybindingsServiceOverride(),
-    ...getTextmateServiceOverride(),
-    ...getThemeServiceOverride(),
-    ...getLanguagesServiceOverride(),
-    ...getAudioCueServiceOverride(),
-    ...getDebugServiceOverride(),
-    ...getPreferencesServiceOverride(),
-    ...getViewsServiceOverride(openNewCodeEditor),
-    ...getBannerServiceOverride(),
-    ...getStatusBarServiceOverride(),
-    ...getTitleBarServiceOverride(),
-    ...getSnippetServiceOverride(),
-    ...getQuickAccessServiceOverride({
-        isKeybindingConfigurationVisible: isEditorPartVisible,
-        shouldUseGlobalPicker: (_editor, isStandalone) =>
-            !isStandalone && isEditorPartVisible()
-    }),
-    ...getOutputServiceOverride(),
-    ...getSearchServiceOverride(),
-    ...getMarkersServiceOverride(),
-    ...getAccessibilityServiceOverride(),
-    ...getLanguageDetectionWorkerServiceOverride(),
-    ...getStorageServiceOverride(),
-    ...getLifecycleServiceOverride(),
-    ...getEnvironmentServiceOverride({
-        remoteAuthority,
-        enableWorkspaceTrust: true
-    }),
-    ...getWorkspaceTrustOverride()
-})
-StandaloneServices.get(ILogService).setLevel(LogLevel.Off)
+export async function initMonacoServiceOverride() {
+    // Override services
+    await initializeMonacoService({
+        ...getExtensionServiceOverride(toWorkerConfig(ExtensionHostWorker)),
+        ...getModelServiceOverride(),
+        ...getNotificationServiceOverride(),
+        ...getDialogsServiceOverride(),
+        ...getConfigurationServiceOverride(
+            remotePath == null
+                ? monaco.Uri.file('/tmp')
+                : {
+                      id: 'remote-workspace',
+                      uri: monaco.Uri.from({
+                          scheme: 'vscode-remote',
+                          path: remotePath,
+                          authority: remoteAuthority
+                      })
+                  }
+        ),
+        ...getKeybindingsServiceOverride(),
+        ...getTextmateServiceOverride(),
+        ...getThemeServiceOverride(),
+        ...getLanguagesServiceOverride(),
+        ...getAudioCueServiceOverride(),
+        ...getDebugServiceOverride(),
+        ...getPreferencesServiceOverride(),
+        ...getViewsServiceOverride(openNewCodeEditor),
+        ...getBannerServiceOverride(),
+        ...getStatusBarServiceOverride(),
+        ...getTitleBarServiceOverride(),
+        ...getSnippetServiceOverride(),
+        ...getQuickAccessServiceOverride({
+            isKeybindingConfigurationVisible: isEditorPartVisible,
+            shouldUseGlobalPicker: (_editor, isStandalone) =>
+                !isStandalone && isEditorPartVisible()
+        }),
+        ...getOutputServiceOverride(),
+        ...getSearchServiceOverride(),
+        ...getMarkersServiceOverride(),
+        ...getAccessibilityServiceOverride(),
+        ...getLanguageDetectionWorkerServiceOverride(),
+        ...getStorageServiceOverride(),
+        ...getLifecycleServiceOverride(),
+        ...getEnvironmentServiceOverride({
+            remoteAuthority,
+            enableWorkspaceTrust: true
+        }),
+        ...getWorkspaceTrustOverride()
+    })
+    StandaloneServices.get(ILogService).setLevel(LogLevel.Off)
+
+    await initializeVscodeExtensions()
+}
 
 export async function clearStorage(): Promise<void> {
     await ((await getService(IStorageService)) as BrowserStorageService).clear()
 }
 
-await initializeVscodeExtensions()
+await initMonacoServiceOverride()
